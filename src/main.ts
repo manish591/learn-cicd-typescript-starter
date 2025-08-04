a37cab1999c768a9aabb25189bd0f25dcec63e41
Manish Devrani <manishdevrani777@gmail.com>
2025-08-04 22:40:49 +0530

    updated api

--- src/main.ts
+++ src/main.ts
@@ -44,6 +44,12 @@ v1Router.get("/healthz", handlerReadiness);
 
 app.use("/v1", v1Router);
 
+app.use("/healthcheck", (req, res) => {
+  res.json({
+    message: "success"
+  })
+})
+
 app.listen(config.api.port, () => {
   console.log(`Server is running on port: ${config.api.port}`);
 });
